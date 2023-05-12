bc4d8119395e92fbbb0bf08977e5cc0a6710e52a
Diego De Panis <9466288+diegomics@users.noreply.github.com>
2023-05-12 15:28:41 +0200

    Delete 3.GC_Telo directory

--- 3.GC_Telo/2.install_gc_dependencies.sh
+++
@@ -1,7 +0,0 @@
-source 1.XXXXX_variables.cnf
-
-echo ""
-echo "* creating conda environment..."
-echo ""
-export PATH="${CONDA_BIN_DIR}:${PATH}"
-conda create -n GC_env -y -c bioconda -c conda-forge r-base bioconductor-karyoploter r-cowplot seqkit
