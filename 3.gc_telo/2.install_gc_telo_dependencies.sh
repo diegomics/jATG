a8b0573714216e234109a7969b2ca62b9415409c
Diego De Panis <9466288+diegomics@users.noreply.github.com>
2023-05-18 12:13:35 +0200

    Update 2.install_gc_telo_dependencies.sh

--- 3.gc_telo/2.install_gc_telo_dependencies.sh
+++ 3.gc_telo/2.install_gc_telo_dependencies.sh
@@ -4,5 +4,4 @@ echo ""
 echo "* creating conda environment..."
 echo ""
 export PATH="${CONDA_BIN_DIR}:${PATH}"
-conda create -n GC_TELO_env -y -c conda-forge r-base bioconductor-karyoploter r-cowplot seqtk tidk 
-#conda create -n GC_TELO_env -y -c bioconda -c conda-forge r-base bioconductor-karyoploter r-cowplot seqkit tidk
+conda create -n GC_TELO_env -y -c conda-forge r-base bioconductor-karyoploter r-cowplot seqtk seqkit tidk
