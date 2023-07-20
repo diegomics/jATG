a0f6383ff006165d4b0941d572deb2379b0e4fb4
Diego De Panis <9466288+diegomics@users.noreply.github.com>
2023-07-20 15:59:46 +0200

    Update search_sex_scaffolds.py

--- 1.stats/scripts/search_sex_scaffolds.py
+++ 1.stats/scripts/search_sex_scaffolds.py
@@ -1,11 +1,9 @@
-import ssl # this should be fixed somehow 
-ssl._create_default_https_context = ssl._create_unverified_context # this should be fixed somehow 
-
 import argparse
+import csv
 from Bio import Entrez
 
 def get_genes_linked_to_sex_chromosomes(species, email):
-    Entrez.email = email  # Always tell NCBI who you are
+    Entrez.email = email
     species_formatted = species.replace('_', ' ')
     query = f"{species_formatted}[Orgn] AND (X[Chromosome] OR Y[Chromosome] OR Z[Chromosome] OR W[Chromosome])"
 
@@ -18,19 +16,27 @@ def get_genes_linked_to_sex_chromosomes(species, email):
     fasta_sequences = []
 
     for gene_id in gene_ids:
-        handle = Entrez.efetch(db="gene", id=gene_id, rettype="xml")
+        handle = Entrez.efetch(db="gene", id=str(gene_id), rettype="xml")
         gene_record = Entrez.read(handle)
         handle.close()
         gene_info = gene_record[0]['Entrezgene_gene']['Gene-ref']
-        gene_output = f"{gene_info.get('Gene-ref_locus', 'N/A')}\nInterim Symbol: {gene_info.get('Gene-ref_locus', 'N/A')} and Name: {gene_info.get('Gene-ref_desc', 'N/A')} [{species_formatted}]"
-        genes.append(gene_output)
+        gene_loc = "Chromosome location not found"
+        for commentary in gene_record[0].get('Entrezgene_locus', []):
+            label = commentary.get('Gene-commentary_label', '')
+            if 'Chromosome' in label:
+                gene_loc = label
+                break
+        gene_dict = {
+            'Symbol': gene_info['Gene-ref_locus'],
+            'Name': gene_info['Gene-ref_desc'],
+            'Location': gene_loc
+        }
+        genes.append(gene_dict)
 
-        # Link the gene ID to corresponding protein IDs
-        link_handle = Entrez.elink(dbfrom="gene", db="protein", id=gene_id)
+        link_handle = Entrez.elink(dbfrom="gene", db="protein", id=str(gene_id))
         link_record = Entrez.read(link_handle)
         link_handle.close()
 
-        # Fetch the nucleotide sequence for each linked protein ID
         for link in link_record[0]["LinkSetDb"][0]["Link"]:
             prot_id = link["Id"]
             seq_handle = Entrez.efetch(db="protein", id=prot_id, rettype="fasta", retmode="text")
@@ -53,9 +59,11 @@ def main():
     genes, fasta_sequences = get_genes_linked_to_sex_chromosomes(args.species, args.email)
 
     if genes:
-        with open(f"{args.species}_sexChrGenes.txt", 'w') as f:
+        with open(f"{args.species}_sexChrGenes.tsv", 'w', newline='') as f:
+            writer = csv.DictWriter(f, fieldnames=['Symbol', 'Name', 'Location'], delimiter='\t')
+            writer.writeheader()
             for gene in genes:
-                f.write(gene + "\n\n")
+                writer.writerow(gene)
 
         with open(f"{args.species}_sexChrSeqs.faa", 'w') as f:
             for fasta_sequence in fasta_sequences:
@@ -63,4 +71,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
