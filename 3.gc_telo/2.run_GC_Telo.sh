eed30e1f6f866dc9703cce92f821f4d5bbc34d27
Diego De Panis <9466288+diegomics@users.noreply.github.com>
2023-08-30 17:56:46 +0200

    Update 2.run_GC_Telo.sh

--- 3.gc_telo/2.run_GC_Telo.sh
+++ 3.gc_telo/2.run_GC_Telo.sh
@@ -1,6 +1,15 @@
 source $(dirname $PWD)/0.general_variables.cnf
 source 1.gc_variables.cnf
 
+if [ -z "${GENOME_TABLE}" ]
+then
+        echo -e "Using previously obtained file number_lengths_GC_Ns for downstream analysis"
+        export GENOME_TABLE="${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/1.stats/number_lengths_GC_Ns"
+else
+        echo -e "Using provided ${GENOME_TABLE} for downstream analysis"
+fi
+
+
 echo ""
 echo "=== Sending jobs for GC and Telomeres analysis ====================================="
 echo ""
