a279784761cd9ba80df86d14e76ebfc2bf56a8ff
Diego De Panis <9466288+diegomics@users.noreply.github.com>
2023-09-01 17:39:35 +0200

    Update 2.run_GC_Telo.sh

--- 3.gc_telo/2.run_GC_Telo.sh
+++ 3.gc_telo/2.run_GC_Telo.sh
@@ -16,6 +16,6 @@ echo ""
 
 mkdir -p ${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/3.gc_telo/logs
 
-GC_JOB=$(sbatch --mail-user=${USER_MAIL} --mail-type=${MAIL_TYPE} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/3.gc_telo/logs/%x.%j.out --error=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/3.gc_telo/logs/%x.%j.err slurm/3.gc_telo.job)
+GC_JOB=$(sbatch --mail-user=${USER_MAIL} --mail-type=${MAIL_TYPE} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/3.gc_telo/logs/%x.%j.out --error=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/3.gc_telo/logs/%x.%j.err slurm/gc_telo.job)
 GC_JOB_ID=$(echo ${GC_JOB} | cut -d ' ' -f4)
 
