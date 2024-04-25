905ee791122b562c0c404555e3484507de9a416b
Diego De Panis <9466288+diegomics@users.noreply.github.com>
2024-04-25 23:39:22 +0200

    Update OPTIONAL_rerun.sh

--- 5.het_roh/OPTIONAL_rerun.sh
+++ 5.het_roh/OPTIONAL_rerun.sh
@@ -5,6 +5,6 @@ echo ""
 echo "=== Sending jobs for extra step:  ====================================="
 echo ""
 
-DARW_PLOT_JOB=$(sbatch --mail-user=${USER_MAIL} --mail-type=${MAIL_TYPE} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/${SAMPLE_NAME}/5.het_roh.w${WIN_SIZE}/logs/%x.%j.out --error=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/${SAMPLE_NAME}/5.het_roh.w${WIN_SIZE}/logs/%x.%j.err slurm/Darwindow_plot.job)
+DARW_PLOT_JOB=$(sbatch --mail-user=${USER_MAIL} ${SLURM_VARS} --output=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/${SAMPLE_NAME}/5.het_roh.w${WIN_SIZE}/logs/%x.%j.out --error=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/${SAMPLE_NAME}/5.het_roh.w${WIN_SIZE}/logs/%x.%j.err slurm/Darwindow_plot.job)
 DARW_PLOT_JOB_ID=$(echo ${DARW_PLOT_JOB} | cut -d ' ' -f4)
 
