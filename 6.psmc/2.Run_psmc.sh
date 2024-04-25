79a5947fc077dbba47024ea2450104547ff28aea
Diego De Panis <9466288+diegomics@users.noreply.github.com>
2024-04-25 23:40:06 +0200

    Update 2.Run_psmc.sh

--- 6.psmc/2.Run_psmc.sh
+++ 6.psmc/2.Run_psmc.sh
@@ -44,7 +44,7 @@ echo ""
 
 mkdir -p ${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/${SAMPLE_NAME}/6.psmc/logs
 
-CONSENSUS_JOB=$(sbatch --mail-user=${USER_MAIL} --mail-type=${MAIL_TYPE} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/${SAMPLE_NAME}/6.psmc/logs/%x.%j.out --error=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/${SAMPLE_NAME}/6.psmc/logs/%x.%j.err slurm/Consensus.job)
+CONSENSUS_JOB=$(sbatch ${SLURM_VARS} --output=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/${SAMPLE_NAME}/6.psmc/logs/%x.%j.out --error=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/${SAMPLE_NAME}/6.psmc/logs/%x.%j.err slurm/Consensus.job)
 CONSENSUS_JOB_ID=$(echo ${CONSENSUS_JOB} | cut -d ' ' -f4)
 
 
@@ -52,7 +52,7 @@ echo ""
 echo "=== Sending jobs for step 2/4:  ====================================="
 echo ""
 
-CONVERT_JOB=$(sbatch --dependency=afterok:${CONSENSUS_JOB_ID} --mail-user=${USER_MAIL} --mail-type=${MAIL_TYPE} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/${SAMPLE_NAME}/6.psmc/logs/%x.%j.out --error=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/${SAMPLE_NAME}/6.psmc/logs/%x.%j.err slurm/Convert.job)
+CONVERT_JOB=$(sbatch --dependency=afterok:${CONSENSUS_JOB_ID} ${SLURM_VARS} --output=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/${SAMPLE_NAME}/6.psmc/logs/%x.%j.out --error=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/${SAMPLE_NAME}/6.psmc/logs/%x.%j.err slurm/Convert.job)
 CONVERT_JOB_ID=$(echo ${CONVERT_JOB} | cut -d ' ' -f4)
 
 
@@ -61,7 +61,7 @@ echo ""
 echo "=== Sending jobs for step 3/4:  ====================================="
 echo ""
 
-PSMC_JOB=$(sbatch --dependency=afterok:${CONVERT_JOB_ID} --mail-user=${USER_MAIL} --mail-type=${MAIL_TYPE} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/${SAMPLE_NAME}/6.psmc/logs/%x.%j.out --error=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/${SAMPLE_NAME}/6.psmc/logs/%x.%j.err slurm/PSMC.job)
+PSMC_JOB=$(sbatch --dependency=afterok:${CONVERT_JOB_ID} ${SLURM_VARS} --output=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/${SAMPLE_NAME}/6.psmc/logs/%x.%j.out --error=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/${SAMPLE_NAME}/6.psmc/logs/%x.%j.err slurm/PSMC.job)
 PSMC_JOB_ID=$(echo ${PSMC_JOB} | cut -d ' ' -f4)
 
 
@@ -70,5 +70,5 @@ echo ""
 echo "=== Sending jobs for step 4/4:  ====================================="
 echo ""
 
-PLOT_JOB=$(sbatch --dependency=afterok:${PSMC_JOB_ID} --mail-user=${USER_MAIL} --mail-type=${MAIL_TYPE} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/${SAMPLE_NAME}/6.psmc/logs/%x.%j.out --error=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/${SAMPLE_NAME}/6.psmc/logs/%x.%j.err slurm/Plot.job)
+PLOT_JOB=$(sbatch --dependency=afterok:${PSMC_JOB_ID} ${SLURM_VARS} --output=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/${SAMPLE_NAME}/6.psmc/logs/%x.%j.out --error=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/${SAMPLE_NAME}/6.psmc/logs/%x.%j.err slurm/Plot.job)
 PLOT_JOB_ID=$(echo ${PLOT_JOB} | cut -d ' ' -f4)
