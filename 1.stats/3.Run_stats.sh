f859458decf377bb39db8d42dfde2526b05d4cc6
Diego De Panis <9466288+diegomics@users.noreply.github.com>
2024-04-27 20:12:30 +0200

    Update 3.Run_stats.sh

--- 1.stats/3.Run_stats.sh
+++ 1.stats/3.Run_stats.sh
@@ -7,7 +7,7 @@ echo ""
 
 
 mkdir -p ${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/1.stats/logs
-STATS_JOB=$(sbatch --mail-user=${USER_MAIL} --mail-type=${MAIL_TYPE} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/1.stats/logs/%x.%j.out --error=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/1.stats/logs/%x.%j.err slurm/Stats.job)
+STATS_JOB=$(sbatch ${SLURM_VARS} --output=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/1.stats/logs/%x.%j.out --error=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/1.stats/logs/%x.%j.err slurm/Stats.job)
 STATS_JOB_ID=$(echo ${STATS_JOB} | cut -d ' ' -f4)
 
 
@@ -21,7 +21,7 @@ else
 	echo "=== Sending jobs for step: DotPlot ====================================="
 	echo ""
 
-	DOTPLOT_JOB=$(sbatch --dependency=afterok:${STATS_JOB_ID} --mail-user=${USER_MAIL} --mail-type=${MAIL_TYPE} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/1.stats/logs/%x.%j.out --error=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/1.stats/logs/%x.%j.err slurm/DotPlot.job)
+	DOTPLOT_JOB=$(sbatch --dependency=afterok:${STATS_JOB_ID} ${SLURM_VARS} --output=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/1.stats/logs/%x.%j.out --error=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/1.stats/logs/%x.%j.err slurm/DotPlot.job)
 	DOTPLOT_JOB_ID=$(echo ${DOTPLOT_JOB} | cut -d ' ' -f4)
 
 fi
@@ -37,7 +37,7 @@ else
         echo "=== Sending jobs for step: Sex Check sex-chromosome linked genes ====================================="
         echo ""
 
-	SEX_CHROM_JOB=$(sbatch --dependency=afterok:${STATS_JOB_ID} --mail-user=${USER_MAIL} --mail-type=${MAIL_TYPE} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/1.stats/logs/%x.%j.out --error=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/1.stats/logs/%x.%j.err slurm/Sex_chrom.job)
+	SEX_CHROM_JOB=$(sbatch --dependency=afterok:${STATS_JOB_ID} ${SLURM_VARS} --output=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/1.stats/logs/%x.%j.out --error=${OUT_DIR}/jATG/${SPECIES_NAME}/${ASSEMBLY_ID}/1.stats/logs/%x.%j.err slurm/Sex_chrom.job)
 	SEX_CHROM_JOB_ID=$(echo ${SEX_CHROM_JOB} | cut -d ' ' -f4)
 
 fi
